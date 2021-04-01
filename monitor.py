eb16bb25ab3d6d81320d3cc9bc9f563a1b42967a
Kevin Lacker <lacker@gmail.com>
2021-04-01 11:14:17 -0700

    some dir mon stuff

--- monitor.py
+++ monitor.py
@@ -5,9 +5,21 @@ from flask import Flask
 
 BASE_DIR = os.path.expanduser("~/target_logs")
 
+
+def list_files():
+    """Return a list of (filename, filesize, modification time) for the files in the base dir."""
+    answer = []
+    for fname in os.listdir(BASE_DIR):
+        full_path = os.path.join(BASE_DIR, fname)
+        if os.path.isfile(full_path):
+            stat = os.stat(full_path)
+            answer.append((fname, stat.st_size, stat.st_mtime))
+    return answer
+
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "welcome to monitor"
+    return f"welcome to monitor: {list_files()}"
