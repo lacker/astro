1c0e47077931966b2374f6451b2d9745095e2b8d
Kevin Lacker <lacker@gmail.com>
2021-04-05 10:30:36 -0700

    hook up gpu

--- script.py
+++ script.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+
+import time
+
 from turbo_seti.find_doppler.find_doppler import FindDoppler
 
 file = "data/single_coarse_guppi_59046_80036_DIAG_VOYAGER-1_0011.rawspec.0000.h5"
@@ -7,5 +11,9 @@ doppler = FindDoppler(
     max_drift=4,  # Max drift rate = 4 Hz/second
     snr=10,  # Minimum signal to noise ratio = 10:1
     out_dir="output",  # This is where the turboSETI output files will be stored.
+    gpu_backend=True,
 )
+start = time.time()
 doppler.search()
+elapsed = time.time() - start
+print(f"time elapsed: {elapsed:.2f}s")
