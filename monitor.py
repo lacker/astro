9ee74237d95b913bf82d63442c47b0aeeb90a3b1
Kevin Lacker <lacker@gmail.com>
2021-04-06 15:21:31 -0700

    tweak monitor script

--- monitor.py
+++ monitor.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 
 import os
+import sys
 from flask import Flask
 
-BASE_DIR = os.path.expanduser("~/target_logs")
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(THIS_DIR, "data")
 
-
-def list_files():
-    """Return a list of (filename, filesize, modification time) for the files in the base dir."""
+def list_data_files():
+    """Return a list of (filename, filesize, modification time) for the files in the data dir."""
     answer = []
-    for fname in os.listdir(BASE_DIR):
-        full_path = os.path.join(BASE_DIR, fname)
+    for fname in os.listdir(DATA_DIR):
+        full_path = os.path.join(DATA_DIR, fname)
         if os.path.isfile(full_path):
             stat = os.stat(full_path)
             answer.append((fname, stat.st_size, stat.st_mtime))
@@ -19,7 +20,8 @@ def list_files():
 
 app = Flask(__name__)
 
+app.logger.info(f"{DATA_DIR=}")
 
 @app.route("/")
 def index():
-    return f"welcome to monitor: {list_files()}"
+    return f"welcome to monitor: {list_data_files()}"
