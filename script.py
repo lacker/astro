f260d32f6751e633b1584e176fe6fb9c3aecaf05
Kevin Lacker <lacker@gmail.com>
2021-04-07 12:04:17 -0700

    use a larger test file

--- script.py
+++ script.py
@@ -4,10 +4,11 @@ import time
 
 from turbo_seti.find_doppler.find_doppler import FindDoppler
 
-file = "data/single_coarse_guppi_59046_80036_DIAG_VOYAGER-1_0011.rawspec.0000.h5"
+# file = "single_coarse_guppi_59046_80036_DIAG_VOYAGER-1_0011.rawspec.0000.h5"
+file = "blc41_guppi_59103_01372_PSR_B2021+51_0009.rawspec.0000.h5"
 
 doppler = FindDoppler(
-    file,
+    "data/" + file,
     max_drift=4,  # Max drift rate = 4 Hz/second
     snr=10,  # Minimum signal to noise ratio = 10:1
     out_dir="output",  # This is where the turboSETI output files will be stored.
@@ -16,4 +17,4 @@ doppler = FindDoppler(
 start = time.time()
 doppler.search()
 elapsed = time.time() - start
-print(f"time elapsed: {elapsed:.2f}s")
+print(f"turboseti time elapsed: {elapsed:.2f}s")
