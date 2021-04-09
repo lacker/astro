1af85dd08bc17202dfe99cc1fe4b76c4940cc1ed
Kevin Lacker <lacker@gmail.com>
2021-04-09 10:35:22 -0700

    fetch.py

---
+++ fetch.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+"""
+Fetches data from the Open Data Archive.
+"""
+
+from astropy.time import Time
+import requests
+import sys
+
+API = "http://35.236.84.6:5001/api"
+
+now = Time.now().mjd
+params = {"cadence": False, "target": "", "time-start": now - 300}
+json = requests.get(f"{API}/query-files", params=params).json()
+if json["result"] != "success":
+    print(json, file=sys.stderr)
+    raise IOError("API failure")
+data = json["data"]
+
+# Sort by most recent first
+data.sort(key=lambda x: -x["mjd"])
+print(f"fetched {len(data)} entries. most recent:")
+for entry in data[:10]:
+    print(entry)
---
+++ hyper.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from hyperseti import hyperseti
+
+fname = "data/blc41_guppi_59103_01372_PSR_B2021+51_0009.rawspec.0000.h5"
+
+hits = hyperseti.find_et_serial(
+    fname, filename_out="output/hits.csv", gulp_size=2 ** 18, max_dd=1.0, threshold=50
+)
