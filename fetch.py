56a8d74abe160009d65210ac363b9b14954cdc31
Kevin Lacker <lacker@gmail.com>
2021-04-12 15:33:15 -0700

    grab good candidates

--- fetch.py
+++ fetch.py
@@ -9,6 +9,7 @@ import sys
 
 # API = "http://35.236.84.6:5001/api"
 API = "http://seti.berkeley.edu/opendata/api"
+GIGABYTE = 1024 ** 3
 
 # Find HDF5 files from the last year
 now = Time.now().mjd
@@ -17,7 +18,12 @@ json = requests.get(f"{API}/query-files", params=params).json()
 if json["result"] != "success":
     print(json, file=sys.stderr)
     raise IOError("API failure")
-data = json["data"]
+
+# At most 5 gig in size
+data = [x for x in json["data"] if x["size"] <= 5 * GIGABYTE]
+
+# Drop the 0001.h5, those are high-time-resolution / low-frequency-resolution
+data = [x for x in data if not x["url"].endswith("0001.h5")]
 
 # Sort by most recent first
 data.sort(key=lambda x: -x["mjd"])
