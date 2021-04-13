60a0d0e29dece26f9e49d5bb01cbe740f82b43a6
Kevin Lacker <lacker@gmail.com>
2021-04-13 11:52:58 -0700

    profiling

--- hyper.py
+++ hyper.py
@@ -2,8 +2,20 @@
 
 from hyperseti import hyperseti
 
-fname = "data/blc41_guppi_59103_01372_PSR_B2021+51_0009.rawspec.0000.h5"
 
-hits = hyperseti.find_et_serial(
-    fname, filename_out="output/hits.csv", max_dd=1.0, threshold=50
-)
+def main():
+    fname = "data/blc41_guppi_59103_01372_PSR_B2021+51_0009.rawspec.0000.h5"
+
+    hits = hyperseti.find_et_serial(
+        fname, filename_out="output/hits.csv", max_dd=1.0, threshold=50
+    )
+
+
+def profile():
+    import cProfile
+
+    cProfile.run("main()", "prof.hyper")
+
+
+if __name__ == "__main__":
+    profile()
