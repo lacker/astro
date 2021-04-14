15a7d8876b92479ca55fb182939a7edf8c79f6af
Kevin Lacker <lacker@gmail.com>
2021-04-14 16:25:53 -0700

    more cudaish peak finding algorithm

--- peak.py
+++ peak.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python
 
 import cupy as cp
+import cupyx.scipy.ndimage
+import numpy as np
 
 if __name__ == "__main__":
-    data = cp.random.rand(100, 100)
-    data[10, 20] = 30
-    data[40, 50] = 60
-    data[70, 80] = 90
+    data = cp.random.rand(10, 10)
+    data[1, 2] = 30
+    data[4, 5] = 60
+    data[7, 8] = 90
+
+    threshold = 1.0
+    # Find maxes
+    maxes = cupyx.scipy.ndimage.maximum_filter(data, size=(5, 5))
+    # Find peaks
+    peaks = cp.logical_and(maxes == data, data >= threshold)
+    with np.printoptions(linewidth=200):
+        print(maxes)
+        print(peaks)
+        print(cp.argwhere(peaks))
