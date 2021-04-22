7a47477793e388d3111362a279ac556ca3a1a0a6
Kevin Lacker <lacker@gmail.com>
2021-04-22 08:53:41 -0700

    make downloader check space

--- fetch.py
+++ fetch.py
@@ -10,7 +10,7 @@ import requests
 import sys
 from urllib.request import urlretrieve
 
-NUM_FILES = 10
+NUM_FILES = 20
 DATA_DIR = "/d/astrodata"
 if not os.path.exists(DATA_DIR):
     print(f"please mount {DATA_DIR}")
@@ -18,6 +18,18 @@ if not os.path.exists(DATA_DIR):
 
 API = "http://seti.berkeley.edu/opendata/api"
 GIGABYTE = 1024 ** 3
+MAX_FILE_GIGS = 5
+
+
+def check_disk_space(desired):
+    # Check free space
+    statvfs = os.statvfs(DATA_DIR)
+    free_bytes = statvfs.f_frsize * statvfs.f_bfree
+    print(f"{free_bytes / GIGABYTE} GB free disk space")
+    if free_bytes < desired:
+        print("please free up more disk space")
+        sys.exit(1)
+
 
 # Find HDF5 files from the last year
 now = Time.now().mjd
@@ -28,7 +40,7 @@ if response["result"] != "success":
     raise IOError("API failure")
 
 # At most 5 gig in size
-data = [x for x in response["data"] if x["size"] <= 5 * GIGABYTE]
+data = [x for x in response["data"] if x["size"] <= MAX_FILE_GIGS * GIGABYTE]
 
 # Drop the 0001.h5, those are high-time-resolution / low-frequency-resolution
 data = [x for x in data if not x["url"].endswith("0001.h5")]
@@ -40,15 +52,17 @@ data = data[:NUM_FILES]
 # Download things
 for entry in data:
     url = entry["url"]
+    size = entry["size"]
     fname = url.split("/")[-1]
     dest = os.path.join(DATA_DIR, fname)
     print(entry)
     if os.path.exists(dest):
-        if os.path.getsize(dest) == entry["size"]:
+        if os.path.getsize(dest) == size:
             print(dest, "already exists")
             continue
         else:
             print(dest, "has weird size??")
+    check_disk_space(size)
     print("downloading", dest)
     urlretrieve(url, dest)
 
