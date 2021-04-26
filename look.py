67a43ddbdffb2b5c56d2ecaa6d9241877c7d01a8
Kevin Lacker <lacker@gmail.com>
2021-04-26 11:47:08 -0700

    analysis

--- look.py
+++ look.py
@@ -4,5 +4,5 @@
 FILE = "/d/astrodata/guppi_59012_79941_6072997821_XaS038-S5-HVS_S_0001.0000.h5"
 from blimpy import Waterfall
 
-data = Waterfall(FILE)
-print(data.info())
+obs = Waterfall(FILE)
+print(obs.info())
