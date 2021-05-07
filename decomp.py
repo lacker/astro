7076446b520972685125d9adfd80e6d69ec547fa
Kevin Lacker <lacker@gmail.com>
2021-05-07 09:25:24 -0700

    h5py ok

--- decomp.py
+++ decomp.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from blimpy import Waterfall
+import h5py
 
 # BASE = "data/blc41_guppi_59103_01372_PSR_B2021+51_0009.rawspec.0000"
 BASE = "../VoyagerTutorialRepository/voyager_2020_data/single_coarse_guppi_59046_80036_DIAG_VOYAGER-1_0011.rawspec.0000"
@@ -8,5 +9,9 @@ BASE = "../VoyagerTutorialRepository/voyager_2020_data/single_coarse_guppi_59046
 H5 = BASE + ".h5"
 DAT = BASE + ".dat"
 
-waterfall = Waterfall(H5)
-print(waterfall.info())
+h5 = h5py.File(H5, mode="r")
+data = h5["data"]
+print(data.attrs)
+
+# waterfall = Waterfall(H5)
+# print(waterfall.info())
