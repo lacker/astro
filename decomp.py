d46c3590bb19081ca21bd5f77b545031f3c34ccc
Kevin Lacker <lacker@gmail.com>
2021-05-12 15:29:18 -0700

    get h5py working independently of blimpy

--- decomp.py
+++ decomp.py
@@ -1,16 +1,8 @@
 #!/usr/bin/env python
 
-from blimpy import Waterfall
+import hdf5plugin
 import h5py
 
-# BASE = "data/blc41_guppi_59103_01372_PSR_B2021+51_0009.rawspec.0000"
-BASE = "../VoyagerTutorialRepository/voyager_2020_data/single_coarse_guppi_59046_80036_DIAG_VOYAGER-1_0011.rawspec.0000"
-
-H5 = BASE + ".h5"
-DAT = BASE + ".dat"
-
-h5 = h5py.File(H5, mode="r")
-print(list(h5.keys()))
-
-# waterfall = Waterfall(H5)
-# print(waterfall.info())
+h5 = h5py.File("data/voyager.h5", mode="r")
+data = h5["data"]
+print(data[0, 0, 0])
