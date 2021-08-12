db7cad753050fe5078eecce12fb1a410d91018ac
Kevin Lacker <lacker@gmail.com>
2021-08-12 08:47:51 -0700

    spike_avg

--- viewer.py
+++ viewer.py
@@ -46,6 +46,7 @@ if __name__ == "__main__":
         print("chunk size is", chunk.shape)
         chunk_mid = chunk.shape[1] // 2
         print("max of chunk left for chunk", i, "is", cupy.max(chunk[:, :chunk_mid]))
+        spike_avg = cupy.mean(chunk[:, chunk_mid])
         print(
             "max of chunk right for chunk",
             i,
