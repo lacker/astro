1c47c045a118bebd29a1e1179ef3601c59569b2d
Kevin Lacker <lacker@gmail.com>
2022-08-30 14:05:49 -0700

    diff metadata

--- diff.py
+++ diff.py
@@ -4,7 +4,6 @@ from blimpy import Waterfall
 import h5py
 import os
 
-assert __name__ == "__main__"
 
 DATA_DIR = os.path.expanduser("~/data/difftest")
 H5 = os.path.join(DATA_DIR, "guppi_59816_50605_000017_J1939-6342_0001.band00.beam00.h5")
@@ -14,3 +13,25 @@ FIL = os.path.join(DATA_DIR, "guppi_59816_50605_000017_J1939-6342.SB00.B00.fil")
 h5 = h5py.File(H5, mode="r")
 fil = Waterfall(FIL)
 
+def force_decode(s):
+    if hasattr(s, "decode"):
+        return s.decode()
+    return s
+
+def approx_equal(a, b):
+    if type(a) is float:
+        return abs(a - b) / abs(a) < 0.01
+    return force_decode(a) == force_decode(b)
+
+# Check metadata
+mismatches = 0
+for key in ["fch1", "foff", "source_name", "tsamp", "tstart"]:
+    h5_val = h5["data"].attrs[key]
+    fil_val = fil.header[key]
+    if not approx_equal(h5_val, fil_val):
+        if mismatches:
+            print()
+        mismatches += 1
+        print(f"mismatch for {key}:")
+        print(f"h5 value: {h5_val}")
+        print(f"fil value: {fil_val}")
