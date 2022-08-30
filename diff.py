7230ce95f2d59b961c29b8735cb51eb5a3ac6da0
Kevin Lacker <lacker@gmail.com>
2022-08-30 11:40:38 -0700

    script to compare two filterbanks

---
+++ diff.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import os
+
+DATA_DIR = os.path.expanduser("~/data/difftest")
+H5 = os.path.join(DATA_DIR, "guppi_59816_50605_000017_J1939-6342_0001.band00.beam00.h5")
+FIL = os.path.join(DATA_DIR, "guppi_59816_50605_000017_J1939-6342.SB00.B00.fil")
+
+assert __name__ == "__main__"
